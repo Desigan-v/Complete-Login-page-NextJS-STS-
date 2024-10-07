222574f48ca3eb611560df3633aea3148648c55b
Desigan-v <desigan1506@gmail.com>
2024-10-07 09:46:58 +0530

    Update MobiApplication.java

--- MobiApplication.java
+++ MobiApplication.java
@@ -1,17 +1,3 @@
-/*package com.example.mobi;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class MobiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MobiApplication.class, args);
-	}
-
-}
-*/
 package com.example.mobi;
 
 import org.springframework.boot.SpringApplication;

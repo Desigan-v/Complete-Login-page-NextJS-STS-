2bf6134c64e376e3c385c801f65f3d14fd999d6a
Desigan-v <desigan1506@gmail.com>
2024-10-07 09:42:55 +0530

    Update UserRepository.java

--- repository/UserRepository.java
+++ repository/UserRepository.java
@@ -1,26 +1,3 @@
-/*package com.example.mobi.repository;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.mobi.user.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
-
-/*
-package com.example.mobi.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.mobi.user.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
-*/
-
 package com.example.mobi.repository;
 
 import java.util.Optional;
